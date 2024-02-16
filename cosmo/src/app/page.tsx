0d996572bd9ed7d98b1f3b9711dd5681392cebf9
Gilber Chen <gilber1002@byui.edu>
2024-02-15 18:14:42 -0700

    slied show

--- cosmo/src/app/page.tsx
+++ cosmo/src/app/page.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image";
+import { Carousel } from 'flowbite-react';
+
+
 
 export default function Home() {
-  return (
-    <main>
-<div>
-  <h1 className="flex justify-center text-4xl mb-10"> COSMO PAGE </h1>
-</div>
 
-    </main>
+  return (
+      <div className="h-screen w-screen relative ">
+      <Carousel>
+        <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+      </Carousel>
+    </div>
+
+
+  
   );
 }
