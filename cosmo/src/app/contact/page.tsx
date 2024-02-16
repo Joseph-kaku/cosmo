49ccea5e626451716076d7c72c6d0b38d090c009
josephkaku <samkaks18@gmail.com>
2024-02-16 11:29:17 -0700

    added a map location

--- cosmo/src/app/contact/page.tsx
+++ cosmo/src/app/contact/page.tsx
@@ -67,6 +67,18 @@ export default function Contact() {
                 <button type="submit">Send</button>
             </form>
             </div>
+            <div>
+    <h1>Location on Map</h1>
+    <iframe
+        srcDoc={`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2879.052609656263!2d-111.79847942442194!3d43.81326814206433!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53540b092fadfe1b%3A0xc746993c26b452ae!2s415%20W%206th%20S%2C%20Rexburg%2C%20ID%2083440!5e0!3m2!1sen!2sus!4v1708107924086!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`}
+        width="600"
+        height="450"
+        style={{ border: '0' }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+    </div>
         </div>
     )
 }
