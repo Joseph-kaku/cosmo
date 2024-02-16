64f3b7a548a9aa5d559ff8d5aaa7b5dbbb0830fa
josephkaku <samkaks18@gmail.com>
2024-02-15 17:19:19 -0700

    added

--- cosmo/src/app/page.tsx
+++ cosmo/src/app/page.tsx
@@ -2,11 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
 <div>
   <h1 className="flex justify-center text-4xl mb-10"> COSMO PAGE </h1>
 </div>
-
-    </main>
   );
 }
