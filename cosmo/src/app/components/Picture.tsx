5b25619d7af8c441ef2b1f713538ac0089f478af
Gilber Chen <gilber1002@byui.edu>
2024-02-29 18:08:40 -0700

    homepage

---
+++ cosmo/src/app/components/Picture.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Button } from "flowbite-react"
+import Image from 'next/image'
+
+export default function Picture({ pic }: { pic: string }) {
+    return (
+        <div className="relative w-screen h-screen overflow-hidden"> 
+            <Image  src={pic} alt="..." layout="fill" objectFit="cover"/>
+            <div className="absolute top-10 ">
+                <p>test</p>
+                <Button>button</Button>
+            </div>
+        </div>
+    )
+}
--- cosmo/src/app/page.tsx
+++ cosmo/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { Carousel } from 'flowbite-react';
+import { Button, Carousel } from 'flowbite-react';
+import Picture from "./components/Picture";
 
 
 
@@ -8,12 +9,17 @@ export default function Home() {
   return (
       <div className="h-screen w-screen relative ">
       <Carousel>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <Picture pic='https://flowbite.com/docs/images/carousel/carousel-1.svg'/>
+        <Picture pic='https://flowbite.com/docs/images/carousel/carousel-2.svg'/>
+        {/* <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <div>Text test </div> */}
+        {/* <Button></Button> */}
       </Carousel>
+      
     </div>
   );
 }
