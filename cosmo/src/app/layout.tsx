468aa43a52221e7d9d5bf0f4ba45a540416cf914
josephkaku <samkaks18@gmail.com>
2024-02-29 19:16:00 -0700

    some changes

--- cosmo/src/app/layout.tsx
+++ cosmo/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Nav from "./components/Nav";
 import "./globals.css";
+import { Footer } from "flowbite-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div>
       <Nav />
-      </div>
         {children}      
+      <Footer />
       </body>
     </html>
   );
