73af07b27104acfeda79634ee9e7ebb202bd60b7
josephkaku <samkaks18@gmail.com>
2024-03-15 08:36:51 -0600

    updates

--- cosmo/src/app/components/Aboutme.tsx
+++ cosmo/src/app/components/Aboutme.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import {Blockquote} from 'flowbite-react'
+import { Rating } from 'flowbite-react'
+
 export default function Aboutme() {
     return (
         <div className="w-screen h-screen">
@@ -10,9 +13,19 @@ export default function Aboutme() {
                 <img className="h-96 w-96 rounded-full mt-5" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt=""/>
             </div> 
             <div className="flex justify-center ml-40 mr-40">
-                <p className="text-lg m-14">
-                    I am a web developer with a passion for creating beautiful and functional websites. I have experience with HTML, CSS, JavaScript, and React. I am also familiar with Node.js and Express. I am a quick learner and I am always looking for new ways to improve my skills. I am currently looking for a job as a web developer.
-                </p>
+                <Blockquote className='mt-5 text-stone-100 text-lg'>
+                " Welcome to [Your Name]'s Beauty Haven!
+                I'm [Your Name], your dedicated cosmetologist, here to bring out your natural beauty and boost your confidence. 
+                With [X] years of experience and a passion for creativity, 
+                I specialize in hair styling, color treatments, skincare, and makeup application.
+                At my studio, it's all about you. I strive to provide personalized service, 
+                staying updated on the latest trends and products to ensure you leave feeling your absolute best. 
+                Let's work together to create a look that reflects your unique style and personality.
+                I can't wait to meet you and embark on this beauty journey together! "
+                <br></br>
+                Warmly,
+                [Your Name]
+                </Blockquote>
             </div>    
         </div>
     )
--- cosmo/src/app/components/Picture.tsx
+++ cosmo/src/app/components/Picture.tsx
@@ -8,8 +8,8 @@ export default function Picture({ pic }: { pic: string }) {
         <div className="relative w-screen h-screen overflow-hidden"> 
             <Image  src={pic} alt="..." layout="fill" objectFit="cover"/>
             <div className="absolute top-1/2 left-2/3 ">
-                <p>This is the place holder </p>
-                <Button>Learn more</Button>
+                <p>Learn more about me and my journey </p>
+                <Button href="/about" >Learn more</Button>
             </div>
         </div>
     )
