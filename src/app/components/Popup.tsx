77007d741b0eb17e8e1fa1ad5fedac6d60a955ae
josephkaku <samkaks18@gmail.com>
2024-04-02 20:26:58 -0600

    removed tranparent on button

--- src/app/components/Popup.tsx
+++ src/app/components/Popup.tsx
@@ -21,7 +21,7 @@ export default function Popup() {
 
   return (
     <>
-      <Button onClick={() => setOpenModal(true)} className='bg-transparent'>Click me</Button>
+      <Button onClick={() => setOpenModal(true)}>Click me</Button>
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
         <Modal.Header> Want to receive weekly updates? </Modal.Header>
         <Modal.Body>
